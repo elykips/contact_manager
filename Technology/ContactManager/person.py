ce3898d513f28361d9c4097028e42da6f271b876
Elkana Rop <elkanarop@gmail.com>
2017-09-18 11:23:07 +0000

    Commit person

---
+++ Technology/ContactManager/person.py
@@ -0,0 +1,11 @@
+class Person:
+	def __init__(self, name, gender):
+		if(name == "Francis"):
+			raise ValueError("What kind of horrible parent would you name your child Francis?")
+		else:
+			self.name = name
+			self.gender = gender
+
+totally_not_francis = Person("Frank", "M")
+totally_not_francis.name = "Francis"
+print(totally_not_francis.name)
